e6181be2bb7c9b4fab8d7a2b01dd3118606c6adb
yapkotilov <yapkolotilov@gmail.com>
2019-03-14 14:36:48 +0300

    Commented exceptions

--- src/exceptions/DuplicateElemException.java
+++ src/exceptions/DuplicateElemException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/** Означает, что пользовал потребовал создать или добавить элемент, который уже существует.
+ *
+ */
 public class DuplicateElemException extends Exception {
     public DuplicateElemException(String message) {
         super(message);
--- src/exceptions/NoSuchElemException.java
+++ src/exceptions/NoSuchElemException.java
@@ -1,5 +1,8 @@
 package exceptions;
 
+/** Означает, что запрашиваемый элемент не существует.
+ *
+ */
 public class NoSuchElemException extends Exception {
     public NoSuchElemException(String message) {
         super(message);
