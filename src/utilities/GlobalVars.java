af0efcc6b30259acbb3d9bb07b5c6a7616b0c745
yapkotilov <yapkolotilov@gmail.com>
2019-03-14 16:29:20 +0300

    GlobalVars added

---
+++ src/utilities/GlobalVars.java
@@ -0,0 +1,11 @@
+package utilities;
+
+import java.util.logging.Logger;
+
+/** Глобальные переменные для программы.
+ */
+public class GlobalVars {
+    public static final int serverPort = 80;
+
+    private GlobalVars() {}
+}
